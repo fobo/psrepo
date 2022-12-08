88bc7b752dd90e24b60f996a2c5793b29dd6f206
Kevin Diehr <kddiehr@gmail.com>
2022-12-08 15:32:17 -0500

    Added lists

---
+++ src/com/perscholas/hackerrank/addingtwonumbers.java
@@ -0,0 +1,18 @@
+package com.perscholas.hackerrank;
+
+public class addingtwonumbers {
+    /*
+     * Complete the 'addNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. FLOAT a
+     *  2. FLOAT b
+     */
+
+    public static int addNumbers(float a, float b) {
+		return 0;
+    // Write your code here
+
+    }
+}
